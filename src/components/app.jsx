3ea6f8d10eab80678c5282ee29feac0f04bc4887
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-08-03 20:14:00 +0300

    added "fetch users" button to app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import user_actions from "~/actions/user";
 import todo_actions from "~/actions/todo";
 import Todo from "./todo";
 import "~/scss/bootstrap.scss";
@@ -7,21 +8,29 @@ import "~/scss/bootstrap.scss";
 const FetchButton = ({ action, name }) => (
 	<button
 		type="button"
-		className="btn btn-primary"
+		className="btn btn-primary me-3"
 		onClick={action}
 	>
 		{name}
 	</button>
 );
 
-const App = ({ fetchTodos }) => (
-	<div className="container">
+const App = ({ fetchUsers, fetchTodos }) => (
+	<div className="container mt-3">
+		<FetchButton action={fetchUsers} name="Fetch Users" />
 		<FetchButton action={fetchTodos} name="Fetch Todos" />
 		<Todo.List />
 	</div>
 );
 
 const mdtp = (dispatch) => ({
+	fetchUsers: async (e) => {
+		e.target.disabled = true;
+		const response = await fetch("https://jsonplaceholder.typicode.com/users");
+		const data = await response.json();
+		dispatch(user_actions.seed(data));
+		e.target.disabled = false;
+	},
 	fetchTodos: async (e) => {
 		e.target.disabled = true;
 		const response = await fetch("https://jsonplaceholder.typicode.com/todos");

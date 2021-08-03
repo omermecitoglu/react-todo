2f15d1b4d53eb04c63d9f5a0682132a223500f0a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-08-03 19:01:17 +0300

    added redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./core";
 import App from "./components/app";
 
 const body = document.getElementsByTagName("body")[0];
 const root = document.createElement("div");
 root.id = "root";
 body.prepend(root);
-ReactDOM.render(<App />, root);
+ReactDOM.render(<Provider store={store}><App /></Provider>, root);
